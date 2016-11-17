48793aeb82963dd48aecda8d948aec6b1305b6f1
LiZhao12 <437336856@qq.com>
2016-11-17 23:38:31 +0800

    1点改动;请假条大部分功能完成
    Signed-off-by: LiZhao12 <437336856@qq.com>

--- 项目文件/ABE_WEB/src/com/abe/service/iVacateService.java
+++ 项目文件/ABE_WEB/src/com/abe/service/iVacateService.java
@@ -12,13 +12,22 @@ public interface iVacateService {
 	/*
 	 * 写请假条
 	 */
-	public RespVacate createVacate();
+	public RespVacate createVacate(String isId,String uId,String itId,String vContent,String vDate);
 	/*
 	 * 批复请假条
 	 */
-	public RespVacate respsVacate();
+	public RespVacate respsVacate(String vId,String isId,String uId,String itId,String vContent,String vDate,String vResp,String vTime);
 	/*
-	 * 查看请假条
+	 * 查看单条请假条
 	 */
-	public RespVacateAll findVacate();
+	public RespVacate findSingleVacate(String vId);
+	/*
+	 * 分页查看给该班主任/学生的请假条
+	 */
+	public RespVacateAll findOwnerVacate(String uId,String pageNo,String pageSize);
+	/*
+	 * 分页查看所有请假条（是否分区域看后期需求）
+	 */
+	public RespVacateAll findAllVacate(String pageNo,String pageSize);
+
 }
