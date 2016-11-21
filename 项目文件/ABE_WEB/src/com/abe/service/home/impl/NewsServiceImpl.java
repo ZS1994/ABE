f21a54ce6505137ff202b57866173f86f5f0befd
LiZhao12 <437336856@qq.com>
2016-11-21 20:19:10 +0800

    共2点改动，食谱功能修改，新闻模块各个类添加 1，食谱功能修改 2，新闻模块添加
    Signed-off-by: LiZhao12 <437336856@qq.com>

---
+++ 项目文件/ABE_WEB/src/com/abe/service/home/impl/NewsServiceImpl.java
@@ -0,0 +1,8 @@
+package com.abe.service.home.impl;
+
+import com.abe.service.home.iNewsService;
+import com.abe.service.impl.BaseServiceImpl;
+
+public class NewsServiceImpl extends BaseServiceImpl implements iNewsService {
+
+}
