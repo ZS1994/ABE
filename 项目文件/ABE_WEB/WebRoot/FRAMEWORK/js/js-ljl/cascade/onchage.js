d1cbf5736e927b389d874e8b073ab085228ef953
ljl <1163083208@qq.com>
2016-12-03 22:38:44 +0800

    新增教师管理页面，以及一些js.css
    Signed-off-by: ljl <1163083208@qq.com>

---
+++ 项目文件/ABE_WEB/WebRoot/FRAMEWORK/js/js-ljl/cascade/onchage.js
@@ -0,0 +1,47 @@
+function  provincefunction(){
+		 var objS = document.getElementById("sheng");
+	     var text = objS.options[objS.selectedIndex].text;
+		$("#province").val(text);
+	}
+	
+	function  fatherfunction(){
+		 var objS = document.getElementById("shi");
+	     var text = objS.options[objS.selectedIndex].text;
+		$("#father").val(text);
+	}
+	
+	function  areafunction(){
+		 var objS = document.getElementById("xian");
+	        var text = objS.options[objS.selectedIndex].text;
+		$("#area").val(text);
+	}
+	
+	function  streetfunction(){
+		 var objS = document.getElementById("xiang");
+	        var text = objS.options[objS.selectedIndex].text;
+		$("#street").val(text);
+	}
+	
+	function  provincefunction1(){
+		 var objS = document.getElementById("sheng1");
+	     var text = objS.options[objS.selectedIndex].text;
+		$("#province1s").val(text);
+	}
+	
+	function  fatherfunction1(){
+		 var objS = document.getElementById("shi1");
+	     var text = objS.options[objS.selectedIndex].text;
+		$("#father1s").val(text);
+	}
+	
+	function  areafunction1(){
+		 var objS = document.getElementById("xian1");
+	        var text = objS.options[objS.selectedIndex].text;
+		$("#area1s").val(text);
+	}
+	
+	function  streetfunction1(){
+		 var objS = document.getElementById("xiang1");
+	        var text = objS.options[objS.selectedIndex].text;
+		$("#street1s").val(text);
+	}
