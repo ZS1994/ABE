7545078dfcf1aeae1fb633c1cb24aec490044d50
LiZhao12 <437336856@qq.com>
2016-12-07 20:59:10 +0800

    共3点，数据库文件及设计文档，测试接口文档，项目文件反馈模块整体
    Signed-off-by: LiZhao12 <437336856@qq.com>

---
+++ 项目文件/ABE_WEB/src/com/abe/service/information/impl/ReturnServiceImpl.java
@@ -0,0 +1,122 @@
+package com.abe.service.information.impl;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import com.abe.entity.AllInform;
+import com.abe.entity.InfoTeacher;
+import com.abe.entity.Return;
+import com.abe.entity.Users;
+import com.abe.entity.app.RespReturn;
+import com.abe.entity.app.RespReturnAll;
+import com.abe.service.impl.BaseServiceImpl;
+import com.abe.service.information.iReturnService;
+import com.abe.tools.NameOfDate;
+import com.abe.tools.Page;
+
+public class ReturnServiceImpl extends BaseServiceImpl implements
+		iReturnService {
+
+	@Override
+	public RespReturnAll findAllReturnsByPage(String pageNo, String pageSize,
+			String rStatus) {
+		RespReturnAll respReturnAll = new RespReturnAll();
+		Return returns = new Return();
+		int pano = Integer.valueOf(pageNo);
+		int size = Integer.valueOf(pageSize);
+
+		if (pano <= 0) {
+			respReturnAll.setResult("002");
+			respReturnAll.setData(null);
+		} else if (size <= 0) {
+			respReturnAll.setResult("003");
+			respReturnAll.setData(null);
+		} else {
+			Page page = new Page(pano, 0, size);
+			String hql1 = "from Return order by RTime desc ";
+			List<Return> list = query(hql1, null, hql1, page);
+			for (int i = 0; i < list.size(); i++) {
+				Users user = (Users) get(Users.class, list
+						.get(i).getUId());
+				list.get(i).setUser(user);
+			}
+			if (list.size() > 0) {
+				respReturnAll.setData(list);
+				respReturnAll.setResult("001");
+			} else {
+				respReturnAll.setData(null);
+				respReturnAll.setResult("004");
+			}
+		}
+		return respReturnAll;
+	}
+
+	@Override
+	public RespReturn findSingleReturn(String rId) {
+		RespReturn respReturn = new RespReturn();
+		if (rId == null && "".equals(rId)) {
+			respReturn.setData(null);
+			respReturn.setResult("003");
+		} else{
+		Return returns =(Return) get(Return.class,rId);
+		returns.setRStatus("已查看");
+		update(returns);
+		respReturn.setData(returns);
+		respReturn.setResult("001");
+		}
+		return respReturn;
+	}
+
+	@Override
+	public RespReturn insertReturn(String rContent, String uId) {
+		RespReturn respReturn = new RespReturn();
+		Return returns = new Return();
+		if (rContent == null && "".equals(rContent)) {
+			respReturn.setData(null);
+			respReturn.setResult("002");
+		} else if (uId == null && "".equals(uId)) {
+			respReturn.setData(null);
+			respReturn.setResult("003");
+		} else{
+		String time = getTime();
+		NameOfDate nameOfData = new NameOfDate();
+		String rId = nameOfData.getNum();
+		
+		returns.setRContent(rContent);
+		returns.setRId(rId);
+		returns.setUId(uId);
+		returns.setRStatus("未查看");
+		returns.setRTime(time);
+		save(returns);
+		respReturn.setData(returns);
+		respReturn.setResult("001");
+		
+		}
+		return respReturn;
+	}
+
+	@Override
+	public RespReturn updateReturn(String rId, String rStatus) {
+		RespReturn respReturn = new RespReturn();
+		if (rStatus == null && "".equals(rStatus)) {
+			respReturn.setData(null);
+			respReturn.setResult("002");
+		} else if (rId == null && "".equals(rId)) {
+			respReturn.setData(null);
+			respReturn.setResult("003");
+		} else{
+		Return returns =(Return) get(Return.class,rId);
+		returns.setRStatus(rStatus);
+		update(returns);
+		respReturn.setData(returns);
+		respReturn.setResult("001");
+		}
+		return respReturn;
+	}
+	public static String getTime() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String time = sdf.format(new Date());
+		return time;
+	}
+}
